f1ea599ced0d86c0f9aa408b0246dd5ea99f6dd4
Jerbeson Vinycius <jerbesonvinycius@gmail.com>
2021-09-25 20:18:08 -0300

    Mudança de configuraçoes e variaveis de ambiente

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 from flask import Flask
 import os
 
-# load_dotenv() # Carregando todas as variáveis de ambiente
+load_dotenv() # Carregando todas as variáveis de ambiente
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
